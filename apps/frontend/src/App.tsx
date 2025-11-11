286943f19c35f1111bba1f9a2667b880d9a0c9be
Romeo Cavazza <romeo.cavazza@gmail.com>
2025-11-11 22:25:19 +0100

    feat: page Demo pour oEmbed

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -23,6 +23,7 @@ import Projects from "./pages/Projects";
 import ProjectsNew from "./pages/ProjectsNew";
 import ProjectDetail from "./pages/ProjectDetail";
 import CreatorDetail from "./pages/CreatorDetail";
+import Demo from "./pages/Demo";
 
 const queryClient = new QueryClient();
 
@@ -39,6 +40,7 @@ const App = () => (
               <Route path="/auth" element={<Auth />} />
               <Route path="/auth/callback" element={<AuthCallback />} />
               <Route path="/docs" element={<Docs />} />
+              <Route path="/demo" element={<Demo />} />
               {/* Projects Routes */}
               <Route path="/projects" element={<ProtectedRoute><Projects /></ProtectedRoute>} />
               <Route path="/projects/new" element={<ProtectedRoute><ProjectsNew /></ProtectedRoute>} />
